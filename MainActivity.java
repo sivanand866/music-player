6381555d6d0d8578a8f7b075191f66a5f2ce440d
sivanand <84615569+sivanand866@users.noreply.github.com>
2023-08-20 09:37:42 +0530

    Create MainActivity.java

---
+++ MainActivity.java
@@ -0,0 +1,91 @@
+package com.example.muzic;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.Manifest;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionDeniedResponse;
+import com.karumi.dexter.listener.PermissionGrantedResponse;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.single.PermissionListener;
+
+import java.io.File;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ListView lv;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        lv=findViewById(R.id.lv);
+        Dexter.withContext(this)
+                .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
+                .withListener(new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted(PermissionGrantedResponse permissionGrantedResponse) {
+                        ArrayList<File> mysongs=fetchSongs(Environment.getExternalStorageDirectory());
+                        String [] items=new String[mysongs.size()];
+                        for(int i=0;i<mysongs.size();i++)
+                        {
+                            items[i]=" " +
+                                    ""+mysongs.get(i).getName().replace(".mp3","");
+                        }
+                        ArrayAdapter<String> adapter=new ArrayAdapter<String>(MainActivity.this,android.R.layout.simple_list_item_1,items);
+                        lv.setAdapter(adapter);
+                        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                            @Override
+                            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                                Intent intent = new Intent(MainActivity.this,song.class);
+                                String currentsong=lv.getItemAtPosition(position).toString();
+                                intent.putExtra("songlist",mysongs);
+                                intent.putExtra("currentsong",currentsong);
+                                intent.putExtra("position",position);
+                                startActivity(intent);
+                            }
+                        });
+
+                    }
+
+                    @Override
+                    public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
+                        Toast.makeText(MainActivity.this, "Please grant permission to continue", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onPermissionRationaleShouldBeShown(PermissionRequest permissionRequest, PermissionToken permissionToken) {
+                        permissionToken.continuePermissionRequest();
+                    }
+                })
+                .check();
+    }
+    public ArrayList<File> fetchSongs(File file){
+        ArrayList arrayList=new ArrayList();
+        File [] songs= file.listFiles();
+        if(songs!=null){
+            for(File myFile: songs)
+            {
+                if(!myFile.isHidden() && myFile.isDirectory()){
+                    arrayList.addAll(fetchSongs(myFile));
+                }
+                else
+                {
+                    if(myFile.getName().endsWith(".mp3") && !myFile.getName().startsWith(".")){
+                        arrayList.add(myFile);
+                    }
+                }
+            }
+        }
+        return arrayList;
+    }
+}
